e3b39cddcb24023820ef0961ec80533de7e7b114
Filipe <granha.carlos@gmail.com>
2017-08-22 00:46:50 +0100

    added link to icons on left hand side

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -4,7 +4,7 @@ class Movie extends React.Component {
   render () {
     return(
       <tr>
-        <td><img src="https://png.icons8.com/plus/win8/1600" alt="plus" width="40"/></td>
+        <td><a href="http://www.google.com"> <img src="https://png.icons8.com/plus/win8/1600" alt="plus" width="40"/></a></td>
         <td className="movieTitle">{this.props.title}</td>
         <td>{<a className = "showTimes" href="http://www.google.com">Showtimes</a>}</td>
       </tr>
